3e8e4fb0d63aca3c4f714e104de6565baf19159c
kenjyou <frisk.nm2901@icloud.com>
2023-03-12 15:43:53 +0900

    8-5

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 
 class PostController extends Controller
@@ -16,4 +16,31 @@ class PostController extends Controller
     {
         return view('posts/show')->with(['post' => $post]);
     }
+    
+    public function create()
+    {
+        return view('posts/create');
+    }
+    
+    public function store(PostRequest $request, Post $post)
+    {    
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function edit(Post $post)
+    {
+        return view('posts/edit')->with(['post' => $post]);
+    }
+    
+    public function update(PostRequest $request, post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+        return redirect('/posts/' . $post->id);
+    }
 }
+
+
+
